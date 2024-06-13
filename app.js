7c445eed7ab7b25139aee14e6076c0c64f69cb54
Jeff <107892110+arslonga83@users.noreply.github.com>
2024-06-12 21:53:25 -0700

    added bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
